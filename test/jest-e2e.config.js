0a74226be940527cceb6882e7705e464fe1a5554
mts88 <fsca.motisi@gmail.com>
2024-10-01 15:27:13 +0200

    build(package.json): add missing script for e2e tests
    Add script for e2e testing

---
+++ test/jest-e2e.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable unicorn/prefer-module */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable unicorn/prevent-abbreviations */
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('../tsconfig.json');
+
+module.exports = {
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    rootDir: './../',
+    testEnvironment: 'node',
+    preset: 'ts-jest',
+    testRegex: '.(e2e-spec|e2e.test).ts$',
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    setupFilesAfterEnv: ['./jest.setup.js'],
+    modulePaths: [compilerOptions.baseUrl],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: '<rootDir>/',
+    }),
+    clearMocks: true,
+};
